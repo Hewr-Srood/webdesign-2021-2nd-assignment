b3cb7cef601b8159e42f8a0923b2dd12694d9fcd
Ahmad Saman <ahmad.00079821@gmail.com>
2021-01-20 14:37:54 +0300

    adding some js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const tableBody = document.getElementById("table-body");
+const editBtn = document.getElementById("edit-btn");
 let id = 1;
 const count = () => {
   for (let i = 0; i <= id; i++) {
@@ -18,6 +19,7 @@ const createRow = (id, name, phone, email) => {
           class="btn btn-primary"
           data-bs-toggle="modal"
           data-bs-target="#modal3"
+          id="edit-btn"
       >
       <i class="fas fa-edit"></i>
   </button>
@@ -28,8 +30,27 @@ const createRow = (id, name, phone, email) => {
   </td>
 </tr>`;
 };
-
-tableBody.insertAdjacentHTML(
-  "afterend",
-  createRow(id, "hewr", "07508614371", "hewr@email.com")
+const ahmad = {
+  name: "ahmad",
+  phone: "123",
+  email: "example@email.com",
+};
+const ali = {
+  name: "ali",
+  phone: "456",
+  email: "example@email.com",
+};
+const hewr = {
+  name: "hewr",
+  phone: "388",
+  email: "example@email.com",
+};
+const example = [ahmad, ali, hewr];
+console.log(ahmad.name);
+example.forEach((i, v) =>
+  tableBody.insertAdjacentHTML(
+    "beforeend",
+    createRow(v + 1, i.name, i.phone, i.email)
+  )
 );
+editBtn.addEventListener("click", () => {});
